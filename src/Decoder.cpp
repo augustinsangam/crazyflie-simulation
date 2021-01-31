4b4fd79d3b17d8d2ead640a5f0575f732e97c8d6
Mathurin <mathurin.chritin@polymtl.ca>
2021-01-31 17:08:32 +0100

    removed robot_update decode function and improved enum

--- src/Decoder.cpp
+++ src/Decoder.cpp
@@ -1,5 +1,7 @@
 #include "Decoder.hpp"
 
+namespace rj = rapidjson;
+
 Decoder::Decoder() {
 	/* must be the same order as enum in Decoder.hpp */
 	types.emplace_back("UNKNOWN");
@@ -22,8 +24,8 @@ void Decoder::decode(const std::string &msg) {
 	case MESSAGE_TYPE::LAND:
 		std::cout << "Land()" << std::endl;
 		break;
-	case MESSAGE_TYPE::RTUPDATE:
-		decodeRTUpdate(document);
+	case MESSAGE_TYPE::ROBOT_UPDATE:
+		std::cout << "robot_update received" << std::endl;
 		break;
 	case MESSAGE_TYPE::UNKNOWN:
 		std::cout << "Unknown message received" << std::endl;
@@ -38,20 +40,4 @@ MESSAGE_TYPE Decoder::decodeType(const std::string &msg) {
 		}
 	}
 	return MESSAGE_TYPE::UNKNOWN;
-}
-
-void Decoder::decodeRTUpdate(const rj::Document &doc) {
-	const rj::Value &data = doc["data"];
-	assert(data.IsArray());
-	for (rapidjson::Value::ConstValueIterator itr = data.Begin();
-	     itr != data.End(); ++itr) {
-		const rapidjson::Value &droneRTData = *itr;
-		assert(droneRTData.IsObject());
-		for (rapidjson::Value::ConstMemberIterator itr2 =
-		         droneRTData.MemberBegin();
-		     itr2 != droneRTData.MemberEnd(); ++itr2) {
-			std::cout << itr2->name.GetString() << " : "
-			          << itr2->value.GetDouble() << std::endl;
-		}
-	}
 }
--- src/Decoder.hpp
+++ src/Decoder.hpp
@@ -6,14 +6,17 @@
 #include <rapidjson/document.h>
 #include <vector>
 
-enum class MESSAGE_TYPE { UNKNOWN = 0, TAKEOFF = 1, LAND = 2, RTUPDATE = 3 };
-namespace rj = rapidjson;
+enum class MESSAGE_TYPE {
+	UNKNOWN = 0,
+	TAKEOFF = 1,
+	LAND = 2,
+	ROBOT_UPDATE = 3
+};
 
 class Decoder {
 private:
 	std::vector<std::string> types;
 	MESSAGE_TYPE decodeType(const std::string &msg);
-	void decodeRTUpdate(const rj::Document &doc);
 
 public:
 	Decoder();
