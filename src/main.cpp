39d6f52f50bc96fae5d0712586e70efee750da3b
Mathurin <mathurin.chritin@polymtl.ca>
2021-03-23 18:30:21 +0100

    v0.1 of porting

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@
 #include "SharedQueue.hpp"
 #include "Vec4.hpp"
 #include "conn/Conn.hpp"
+#include "exploration/stabilizer_types.hpp"
 #include "gen_buf.hpp"
+#include <bits/stdint-uintn.h>
 #include <cstdint>
 #include <exception>
 #include <iostream>
@@ -17,8 +19,12 @@
 #include <random>
 #include <string>
 #include <thread>
+#include <unistd.h>
 #include <unordered_map>
 #include <utility>
+
+#include "exploration/state_machine.hpp"
+#include "porting/porting.hpp"
 
 #include <spdlog/spdlog.h>
 
@@ -42,6 +48,21 @@
 #include <argos3/plugins/robots/crazyflie/control_interface/ci_crazyflie_distance_scanner_sensor.h>
 
 static uint16_t mainId = 0; // NOLINT
+static const double PI = 3.141582654;
+
+/* Pointer to the position actuator */
+static argos::CCI_QuadRotorPositionActuator *m_pcPropellers{};
+
+/* Pointer to the battery sensor */
+static argos::CCI_BatterySensor *m_pcBattery{};
+
+/* Pointer to the positioning sensor */
+static argos::CCI_PositioningSensor *m_pcPos{};
+
+/* Pointer to the crazyflie distance sensor */
+static argos::CCI_CrazyflieDistanceScannerSensor *m_pcDistance{};
+
+static FlowDeck flow_deck_{};
 
 class CCrazyflieSensing : public argos::CCI_Controller { // NOLINT
 private:
@@ -60,21 +81,8 @@ private:
 	Decoder decoder_;
 	RTStatus rt_status_;
 	Proxy proxy_;
+	exploration::StateMachine sm_;
 	double orientation_ = argos::CRadians::PI_OVER_TWO.GetValue();
-
-	/* Pointer to the position actuator */
-	argos::CCI_QuadRotorPositionActuator *m_pcPropellers{};
-
-	/* Pointer to the battery sensor */
-	argos::CCI_BatterySensor *m_pcBattery{};
-
-	/* Pointer to the positioning sensor */
-	argos::CCI_PositioningSensor *m_pcPos{};
-
-	/* Pointer to the crazyflie distance sensor */
-	argos::CCI_CrazyflieDistanceScannerSensor *m_pcDistance{};
-
-	FlowDeck flow_deck_{};
 
 public:
 	/* Class constructor. */
@@ -85,6 +93,7 @@ public:
 		brain_.setState(brain::idle);
 		std::cout << "drone " << rt_status_.get_name() << " created"
 		          << std::endl;
+		sm_ = exploration::StateMachine();
 	}
 
 	/* Class destructor. */
@@ -98,8 +107,10 @@ public:
 	void Init(argos::TConfigurationNode & /*t_node*/) override {
 		spdlog::set_level(spdlog::level::trace);
 
-		/**/
-
+		// #################################################################
+		sm_.startup();
+		// TODO merge single-connection in next, merge next in porting-v1
+		// #################################################################
 		m_pcPropellers = GetActuator<argos::CCI_QuadRotorPositionActuator>(
 		    "quadrotor_position");
 
@@ -128,17 +139,17 @@ public:
 	 * The length of the time step is set in the XML file.
 	 */
 	void ControlStep() override {
+		// #################################################################
+		/* Is that it ? */
+		sm_.iteration_loop();
+
+		// #################################################################
 		// Battery
 		const auto &battery = m_pcBattery->GetReading().AvailableCharge;
 
 		// Position
 		const auto &position = m_pcPos->GetReading().Position;
 		const auto &orientation = m_pcPos->GetReading().Orientation;
-
-		// FlowDeck v2
-		// https://www.bitcraze.io/products/flow-deck-v2/
-		const auto camera_data =
-		    flow_deck_.getInitPositionDelta(position, orientation);
 
 		// Look here for documentation on the distance sensor:
 		// /root/argos3/src/plugins/robots/crazyflie/control_interface/ci_crazyflie_distance_scanner_sensor.h
@@ -187,34 +198,6 @@ public:
 			}
 		}
 		++tick_count_;
-
-		const auto next_move =
-		    brain_.computeNextMove(&camera_data, &sensor_data);
-		if (next_move) {
-			// spdlog::info("next_move (" +
-			// std::to_string(next_move->coords.x()) +
-			//              "," + std::to_string(next_move->coords.y()) + "," +
-			//              std::to_string(next_move->coords.z()) + ")" + " yaw
-			//              " + std::to_string(next_move->yaw));
-			if (next_move->relative) {
-				m_pcPropellers->SetRelativePosition(argos::CVector3(
-				    next_move->coords.x(), next_move->coords.y(),
-				    next_move->coords.z()));
-			} else {
-				m_pcPropellers->SetAbsolutePosition(argos::CVector3(
-				    next_move->coords.x(), next_move->coords.y(),
-				    next_move->coords.z()));
-				m_pcPropellers->SetAbsoluteYaw(argos::CRadians(next_move->yaw));
-			}
-		}
-	}
-
-	std::string trunc(float val, int numDigits) {
-		std::string output = std::to_string(val).substr(0, numDigits + 1);
-		if (output.find('.') == std::string::npos || output.back() == '.') {
-			output.pop_back();
-		}
-		return output;
 	}
 
 	/*
@@ -235,7 +218,129 @@ public:
 	 */
 	void Destroy() override { /*conn_->terminate();*/
 	}
+
+	static uint8_t get_ticks_per_sec() { return tick_rate_; }
 };
 
 // NOLINTNEXTLINE
 REGISTER_CONTROLLER(CCrazyflieSensing, "crazyflie_sensing_controller")
+
+// ####################### porting.hpp methods ###########################
+
+uint64_t porting::us_timestamp() {
+	// NOLINTNEXTLINE
+	uint64_t us =
+	    std::chrono::duration_cast<std::chrono::microseconds>(
+	        std::chrono::high_resolution_clock::now().time_since_epoch())
+	        .count();
+	return us;
+}
+
+uint64_t porting::config_block_get_radio_address() {
+	// works only for 1 drone
+	return mainId;
+}
+
+void porting::system_wait_start() {}
+
+void porting::ticks_delay(uint32_t nTicksToDelay) { usleep(nTicksToDelay); }
+
+uint32_t porting::ms_to_ticks(uint16_t ms) {
+	return ms * CCrazyflieSensing::get_ticks_per_sec() / 1000;
+}
+
+void porting::commander_set_setpoint(exploration::setpoint_t *setpoint,
+                                     int priority) {
+	/**
+	 * [Notes]
+	 * setpoint->mode.(x,y,z ou yaw) est soit modeAbs soit modeVelocity, ce qui
+	 correspond en gros à setAbsolute et setRelative. Il y a enfin un
+	 modeDisable pour tout couper
+
+	 z est en modeVelocity pendant le décollage et l'atterrissage, et modeAbs
+	 pendant la plupart du temps ("hover" "vel_command")
+
+
+	 *
+	 */
+}
+
+void porting::estimator_kalman_get_estimated_pos(exploration::point_t *pos) {
+	const auto &position = m_pcPos->GetReading().Position;
+	const auto &orientation = m_pcPos->GetReading().Orientation;
+	CameraData cd = flow_deck_.getInitPositionDelta(position, orientation);
+	pos->x = cd.delta_x;
+	pos->y = cd.delta_y;
+	pos->z = cd.z;
+	pos->timestamp = static_cast<uint32_t>(std::time(nullptr));
+}
+
+bool porting::send_p2p_packet_broadcast(exploration::P2PPacket *p2pp) {
+	// TODO
+}
+
+uint8_t porting::get_deck_bc_multiranger() {
+	return static_cast<uint8_t>(m_pcDistance != nullptr);
+}
+
+uint8_t porting::get_deck_bc_flow2() {
+	// TODO: check for real if the flowdeck is initialized properly
+	return 1U;
+}
+
+float porting::get_kalman_state_z() {
+	const auto &position = m_pcPos->GetReading().Position;
+	const auto &orientation = m_pcPos->GetReading().Orientation;
+	CameraData cd = flow_deck_.getInitPositionDelta(position, orientation);
+	return cd.z;
+}
+
+float porting::get_stabilizer_yaw() {
+	const auto &position = m_pcPos->GetReading().Position;
+	const auto &orientation = m_pcPos->GetReading().Orientation;
+	CameraData cd = flow_deck_.getInitPositionDelta(position, orientation);
+	float_t yaw_rad = cd.yaw;
+	return static_cast<float_t>(yaw_rad * static_cast<double>(180) / PI);
+}
+
+uint8_t porting::get_radio_rssi() {
+	// TODO
+}
+
+float get_distance_sensor(uint8_t sensor_index) {
+	/*
+	 * From ci_crazyflie_distance_scanner_sensor.h
+	 *      front
+	 *
+	 *        0
+	 *
+	 * left 1   3 right
+	 *
+	 *        2
+	 *
+	 *      back
+	 */
+	auto sDistRead = m_pcDistance->GetReadingsMap();
+	auto iterDistRead = sDistRead.begin();
+	if (sDistRead.size() == 4) {
+		for (uint8_t i = 0; i < sensor_index; i++) {
+			iterDistRead++;
+		}
+		return static_cast<float>((iterDistRead++)->second);
+	}
+	float default_value = 0.0;
+	spdlog::error("Unable to read from the multiranger sensors! Returning a "
+	              "default value (" +
+	              std::to_string(default_value) + ").");
+	return default_value;
+}
+
+float porting::get_front_range() { return get_distance_sensor(0); }
+float porting::get_left_range() { return get_distance_sensor(1); }
+float porting::get_back_range() { return get_distance_sensor(2); }
+float porting::get_right_range() { return get_distance_sensor(3); }
+float porting::get_up_range() {
+	// TODO
+	spdlog::error("Up sensor not implemented yet");
+	return 1.0;
+}
