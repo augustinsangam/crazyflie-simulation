c37a3d121ceb4ea337634817a80288ace6ed1a32
Mathurin <mathurin.chritin@polymtl.ca>
2021-04-09 22:45:02 +0200

    added arena selector

--- launch.sh
+++ launch.sh
@@ -1,13 +1,19 @@
 #!/bin/sh -e
 
+# change arena
+ARENA_INDEX=$1
+if [ $# -eq 0 ]
+  then
+	ARENA_INDEX=-1
+fi
+
+python arena_selector.py $ARENA_INDEX
+
 # build the project
 mkdir -p build
 cd build
 cmake -D CMAKE_BUILD_TYPE=Debug ..
 make
-
-# change random seed for obstacle generation
-sed -i "s/random_seed=\".*\" \/>/random_seed=\"$RANDOM\" \/>/g" ../config.xml
 
 # launch the simulation
 argos3 -c ../config.xml
